e4f4c8a1440f656f90282a9dc1473a5e495a2358
MILENA FREITAS DA SILVA ORDONIO <msfo2@discente.ifpe.edu.br>
2022-09-15 16:50:44 -0300

    aulaphp2

--- login.php
+++ login.php
@@ -7,7 +7,25 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+
+<?php
+ 
+ $conexao = mysqli_connect('localhost','root','','pesquisa_DEV');
+ $user = $_POST['usuario'];
+ $pass = $_POST['senha'];
+ $retorno = $conexao->query("SELECT * FROM user WHERE user='$user' AND pass = '$pass';");
+ echo $retorno->num_rows;
+ if ($retorno->num_rows == 1){
+     echo "Seja bem vindo(a)";
+     $user = $retorno->fetch_array();
+     $conexao->query("INSERT INTO ponto (id_user) VALUES (" . $user['id'] . ")");
+ }else{
+     echo "Usuário não autorizado";
+ }
+
+/*
+
+ <?php
     $user = $_POST['usuario'];
     $pass = $_POST['senha'];
     if($user == '123' && $pass == '456'){
@@ -16,5 +34,15 @@
         echo 'usuário não autorizado';
     }
     ?>
+
+echo $conexao->host_info; informações sobre o banco no geral
+
+ echo var_dump($retorno);
+
+ while ($user = $retorno->fetch_array()){
+    echo $user['nome']."<br>"; 
+} */
+?>
+   
 </body>
 </html>
