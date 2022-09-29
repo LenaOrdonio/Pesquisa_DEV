f32a3173fd8e23a26727ceaeac15297e0b2c6ddc
MILENA FREITAS DA SILVA ORDONIO <12332602437@pesqueira.ifpe>
2022-09-29 16:26:05 -0300

    modelo_form

--- form.php
+++ form.php
@@ -8,10 +8,33 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
+<div style="height: 100vh; width: 100vw;" class="d-flex justify-content-center align-items-center">
+        <div class="p-4 mb-4 bg-primary text-dark bg-opacity-10 rounded-4 col-3 d-grid gap-4 border">
+            <img src="volts.logo.png" alt="" class="col-8 offset-2">
+            <form action="" class="d-grid gap-4">
+                <div class="row">
+                    <input type="text" class="form-control" name="usuario" id="usuario" placeholder="insira o nome">
+                </div>
+                <div class="row">
+                    <input type="password" class="form-control" name="senha" id="senha" placeholder="insira sua senha">
+                </div>
+                <div class="row">
+                    <button type="submit" class="btn btn-primary">
+                        Entrar
+                    </button>
+                </div>
 
-    <form action="login.php" method="post">
+            </form>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</html>
+
+
+   <!--  <form action="login.php" method="post" style="height: 100vh;" class="container d-flex justify-content-center">
    
-     <div class="container d-flex justify-content-center">
+     <div class="d-flex justify-content-center" >
     <div class="row gy-2 gx-3 align-items-center">
   <div class="col-auto">
         <input type="text" class="form-control" name="usuario" id="usuario" placeholder="insira o nome">
@@ -19,13 +42,12 @@
   <div class="col-auto">
         <input type="password" class="form-control" name="senha" id="senha" placeholder="insira a senha">
         </div>
-</div>
-
         <button type="submit"  class="btn btn-primary">Entrar</button>
+      </div>
+
+        
   </div>
     </form>
 
-
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-</html>
+    primeiro modelo/tentativa.
+ -->
